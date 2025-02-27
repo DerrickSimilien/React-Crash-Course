ff197500f1546aeb2d74a24146db8d20bbd462e6
Derrick <similienderrick@yahoo.com>
2025-02-27 02:02:14 -0500

    Initialized git repository

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import User from "../components/User.jsx"
+import { Link } from 'react-router-dom'
 
 function Home() {
    const [users, setUsers] = useState([])
@@ -11,24 +13,21 @@ function Home() {
        } 
 
     useEffect(() => {
-        setTimeout(() => {
             fetchUsers();  
-    }, 500);
     }, []); 
-    const pixels = "3px";
-
+    
     return (
         <div>
-     {users.map((user) => {
-          return (
-          <div style={{ border: `${pixels} solid black` }}>
-          <div>{users[0]?.id}</div>
-          <div>{users[0]?.name}</div>
-          <div>{users[0]?.email}</div>
-          <div>{users[0]?.username}</div>
-     </div>
-     );
-     })}
+     {users.map((user) => (
+          <Link to={`/users/${user.id}`} key={user.id}>
+          <User 
+          id={user.id} 
+          name={user.name} 
+          email={user.email} 
+          username={user.username} 
+          />
+        </Link>
+     ))}
     </div>
     );
 }
--- pages/Users.jsx
+++ pages/Users.jsx
@@ -1,9 +1,28 @@
 import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Users() {
-    const { username } = useParams()
+    const { id } = useParams()
+    const [user, setUser] = useState({})
 
-    return <h1>{username}</h1>
+    async function fetchUser() {
+       const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+       setUser(data)
+    }
+
+    useEffect(() => {
+        fetchUser() 
+    }, [])
+    return (
+        <div>
+           <Link to="/">Go back</Link>  
+            <p>{ user.id }</p>
+            <p>{ user.name }</p>
+            <p>{ user.email }</p>
+            <p>{ user.username }</p>
+        </div>
+    )
 }
 
 export default Users 
